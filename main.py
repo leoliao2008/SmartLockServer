50135ff8e37ea214e9563023419ad86c7bb85ac7
leoliao2008 <leoliao2008@163.com>
2019-09-11 11:00:04 -0700

    Initial commit

---
+++ main.py
@@ -0,0 +1,18 @@
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def run_test():
+    return jsonify(data='hello wordl')
+
+
+def run_app():
+    app.run()
+    pass
+
+
+if __name__ == '__main__':
+    print('hello world.')
+    run_app()
